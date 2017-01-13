5ce1e812dab2448ad3f30b74176ab538a78f0a8c
최민진 <brownminjin@naver.com>
2017-01-13 14:12:32 +0900

    초기 버전

--- HelloWorld/src/com/test/hellolworld/helloworld.java
+++ HelloWorld/src/com/test/hellolworld/helloworld.java
@@ -3,5 +3,7 @@ package com.test.hellolworld;
 public class helloworld {
   public static void main(String[] args) {
 	System.out.println("Hello,World");
-}
+	System.out.println("æ»≥Á«œººø‰");
+   System.out.println();
+  }
 }
